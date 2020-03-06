c9e7fdee0cd750f0d13bde242927ac66e06dccce
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-06 13:58:18 +0300

    upp

--- WebSite/check.php
+++ WebSite/check.php
@@ -1,3 +1,8 @@
 <?php
+// print_r($_POST);
+$email = $_POST['email'];
+$message = $_POST['message'];
+if (trim($email) == '') //trim — Удаляет пробелы (или другие символы) из начала и конца строки
+
 
 ?>
