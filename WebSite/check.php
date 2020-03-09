18f35e13694f8104c5401bfa2c02bb71cd6e0bc8
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-09 21:32:41 +0300

    change something

--- WebSite/check.php
+++ WebSite/check.php
@@ -2,7 +2,16 @@
 // print_r($_POST);
 $email = $_POST['email'];
 $message = $_POST['message'];
+
+$error = ''; //сюда запсываем ошибки
 if (trim($email) == '') //trim — Удаляет пробелы (или другие символы) из начала и конца строки
+    $error = 'Enter your Email !';
+else if (trim($message) == '')
+    $error = 'Enter your message';
 
-
-?>
+else if (strlen($message) < 10) // "strlen" - функиция считает количество символов в стоке.
+    $error = 'Сообщение должно быть более 10 символов';
+if ($error != '') {
+    echo $error;
+    exit;  // "exit" — Вывести сообщение и прекратить выполнение текущего скрипта
+}
