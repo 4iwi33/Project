755f0c0e737614ea84c91f4f44e985b597d2e8ea
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-13 11:35:07 +0300

    ch

--- WebSite/auth.php
+++ WebSite/auth.php
@@ -1,5 +1,7 @@
 <?php
-setcookie('user', 'yes',time() + 3600, '/'); //.project-states.000webhostapp.com
+if ($_COOKIE['user'] == 'yes')
+    setcookie('user', 'yes',time() - 3600, '/');
+else
+    setcookie('user', 'yes',time() + 3600, '/'); //.project-states.000webhostapp.com
 
-header('Location: /');
-?>
+header('Location:/');
--- WebSite/check.php
+++ WebSite/check.php
@@ -13,20 +13,18 @@
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $error = ''; //сюда запсываем ошибки
+    $error = ''; // выводит "Enter your Email" если его не ввели
     if (trim($email) == '') //trim — Удаляет пробелы (или другие символы) из начала и конца строки
         $error = 'Enter your Email !';
     else if (trim($message) == '')
-        $error = 'Enter your message';
+        $error = 'Enter your message';//выводит "Enter your message" если его не ввели
 
     else if (strlen($message) < 10) // "strlen" - функиция считает количество символов в стоке.
-        $error = 'Сообщение должно быть более 10 символов';
+        $error = 'Сообщение должно быть более 10 символов';// выводит "Сообщение должно быть более 10 символов" если ввели меньше
     if ($error != '') {
         echo $error;
         exit;  // "exit" — Вывести сообщение и прекратить выполнение текущего скрипта
     }
-
-
 
     // "$subject" - переменая которая содержит название письма
     // "base64_encode" - кодирует данные способом MIME base64.
@@ -37,7 +35,7 @@
     // "mail" - функця для отправки сообщения
     mail('4iwi33@mail.ru', $subject, $headers);
 
-    // header('Location: /about.php');
+      header('Location:/about.php');
     ?>
 </body>
 
